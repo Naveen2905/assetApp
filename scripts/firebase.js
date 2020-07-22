b7596af5082bc0da5440fe1c69ce551ce8e68d6b
Naveen2905 <naveenmalhotra05@gmail.com>
2020-07-21 23:22:56 -0400

    made addAsset html file

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -7,7 +7,7 @@ $(function () {
         authDomain: "asset-tracking-app-29783.firebaseapp.com",
         databaseURL: "https://asset-tracking-app-29783.firebaseio.com",
         projectId: "asset-tracking-app-29783",
-        storageBucket: "asset-tracking-app-29783.appspot.com",
+        storageBucket: "gs://asset-tracking-app-29783.appspot.com/",
         messagingSenderId: "973877790467",
         appId: "1:973877790467:web:aeae67d018753d26504b36",
         measurementId: "G-NBV86DDVCB"
