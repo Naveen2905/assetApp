4a77c684c88462a3518d8d3c7f9ba9f401ae2913
Naveen2905 <naveenmalhotra05@gmail.com>
2020-07-23 15:08:18 -0400

    added media files to storage

--- scripts/addAsset.js
+++ scripts/addAsset.js
@@ -1,7 +1,6 @@
 $(function () {
 
-    // const dbRef = firebase.database().ref();
-    // console.log(dbRef);
+
 
     $('.addAssetInfo').on('submit', function (e) {
         e.preventDefault();
@@ -14,16 +13,15 @@ $(function () {
         const condition = $('#condition').val();
         const warrantyInfo = $('#warrantyInfo').val();
         const returnedDate = $('#returnedDate').val();
-        // const recieptUpload = $('#recieptUpload').val();
         const expiryDate = $('#expiryDate').val();
         const additionalInfo = $('#additionalInfo').val();
         const status = $('#status').val();
         const modelNumber = $('#modelNumber').val();
         const serialNumber = $('#serialNumber').val();
 
-        
+        // Firebase Realtime Database --------------------------- 
         const dbRef = firebase.database().ref();
-        
+
         // creating object to push to database 
         const assetInformation = {
             assetName: assetName,
@@ -41,16 +39,54 @@ $(function () {
             modelNumber: modelNumber,
             serialNumber: serialNumber,
         }
-        
+
         // Pushed to firebase db 
         const firebaseObj = dbRef.push(assetInformation);
 
         //To Reset the form fields
-        $('input[type=text],input[type=number],input[type=date],select').val(''); 
-        
+        $('input[type=text],input[type=number],input[type=date],select').val('');
+
         dbRef.on('value', (data) => {
             console.log(data.val());
         });
+
+    })
+
+    // Firebase Storage ------------------
+
+    const uploader = $('#uploader');
+    const fileButton = $('#fileButton');
+    //Listen for file selection
+
+    fileButton.change(function (e) {
+        //Get the File
+        const file = e.target.files[0];
+        // Create Storage Ref 
+        const storageRef = firebase.storage().ref('reciepts/' + file.name)
+        // Upload File
+        const task = storageRef.put(file);
+        
+        //Update Progress Bar
+        task.on('state_changed',
+
+            function progress(snapshot) {
+                const percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                uploader[0].value = percentage;
+
+            },
+
+            function error(err) {
+
+            },
+
+            function complete() {
+                //After uploading finishes it will reset the input
+                fileButton.val(''); 
+                uploader[0].value = 0;
+                $('#filedrag').text('Success!!');
+            }
+        )
+
     })
 
 
